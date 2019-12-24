54dd58d430a10ead1402bc116eb1cfac20490339
nnthanh2307 <nnthanh.uet@gmail.com>
2019-12-25 01:32:04 +0700

    Air Pollution

--- Air-Pollution-Monitoring-System-master/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con=mysqli_connect("username","root","password","database");
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-} 
-?>
--- Air-Pollution-Monitoring-System-master/phpsqlajax_dbinfo.php
+++
@@ -1,6 +0,0 @@
-<?php
-$username="";
-$password="";
-$database="";
-$con=mysqli_connect("localhost",$username,$password,$database);
-?>
