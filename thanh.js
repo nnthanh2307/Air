c49dcdf9a83ad5f3635abc79b0855478124f8f49
nnthanh2307 <nnthanh.uet@gmail.com>
2020-02-08 17:30:20 +0700

    final commit

--- thanh.js
+++
@@ -1,6 +0,0 @@
-var array = [{firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}, {firstName:"thanh", lastName:"Doe", age:50, eyeColor:"blue"}];
-array.forEach(function(item)
-{
-    console.log(item);
-    //thanh
-})
